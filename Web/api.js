267d60e8c3eb4b91217af9cc4093348d86f93409
zachary <zacharysang@gmail.com>
2017-06-26 22:56:25 -0400

    Began work in api

--- Web/api.js
+++ Web/api.js
@@ -1,10 +1,22 @@
 var express = require('express');
+var Ticket = require('./models/ticket');
+
 var router = express.Router();
 
+// triggered on request to load more
+router.get('/load',function(req,res){
+    var query = Ticket.find();
+    var tickets = query.exec();
+    tickets.then(function(data){
+        res.send(data); // might be able to just pass this function in to the then arguments
+    });
+});
+
 // triggered for any get request to the api
-router.get(function(req,res){
+router.use('*',function(req,res){
+    console.log('\n\ngot here\n\n')
     var responseObj = {
-        foo: bar
+        'foo': 'bar'
     }
     res.send(responseObj);
 });
