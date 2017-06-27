bc788092e7f72a8d60423a04eea17b6016e7e026
zachary <zacharysang@gmail.com>
2017-06-26 22:47:25 -0400

    Added mongoose dependency and ticket model

---
+++ Web/models/ticket.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var ticketSchema = new mongoose.Schema({
+    ticketId: String,
+    problemTitle: String,
+    problemDescription: String,
+    creator: {firstName: String, 
+                lastName: String,
+                contactInfo: String,
+                location: String},
+    creationTime: String,
+    pin: String,
+    Tags:[String],
+    comments: [{name: String, 
+                body:String, 
+                timestamp: String}],
+    status: String,
+    mentorName: String,
+
+});
+
+module.exports = mongoose.model('Ticket',ticketSchema);
