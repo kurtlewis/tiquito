24c5f415fe0d56c7d2e46561b218575a70deae65
zachary <zacharysang@gmail.com>
2017-06-26 23:39:46 -0400

    Added DB component to API

--- Web/api.js
+++ Web/api.js
@@ -1,20 +1,39 @@
+var env = require('./env');
 var express = require('express');
+var mongoose = require('mongoose');
 var Ticket = require('./models/ticket');
 
 var router = express.Router();
 
-// triggered on request to load more
-router.get('/load',function(req,res){
-    var query = Ticket.find();
-    var tickets = query.exec();
-    tickets.then(function(data){
-        res.send(data); // might be able to just pass this function in to the then arguments
-    });
+// Create connection to the DB
+mongoose.connect(env.MONGO_URL,{useMongoClient: true},function(err){
+    if(err){
+        console.log(`mongo had an error :(. error was : ${err}`);
+    }else {
+        console.log('no errors in mongo connection!');
+    }
 });
 
+// Catch mongo runtime errors
+mongoose.connection.on('error',function(err){
+    console.log(`whoops! mongo hit an error: ${err}`);
+});
+
+/*
+router.post('/create',function(req,res){
+
+});
+
+// triggered on request to load more
+router.get('/load',function(req,res){
+    console.log('gonna try load some tickets!\n\n\n\n');
+    var query = Ticket.findOne({},function(err,data){
+        res.send(data);
+    });
+});
+*/
 // triggered for any get request to the api
-router.use('*',function(req,res){
-    console.log('\n\ngot here\n\n')
+router.use('/*',function(req,res){
     var responseObj = {
         'foo': 'bar'
     }
