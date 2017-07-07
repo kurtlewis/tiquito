34e162f82b4f77a704eee4720dc99e84edbef843
Kurt Lewis <kurt.jlewis15@gmail.com>
2017-07-06 20:42:29 -0400

    initial creation of Ticket.java

---
+++ Android/Tiquito/app/src/main/java/com/tiquito/tiquito/Ticket.java
@@ -0,0 +1,120 @@
+package com.tiquito.tiquito;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kurt on 7/6/17.
+ *
+ * Class for controlling general ticket features and loading.
+ */
+
+public class Ticket {
+
+
+    private string id;
+    private string title;
+    private string description;
+    private string creationTime;
+    private string status;
+    private string mentorName;
+    private string creator;
+    private ArrayList<string> tags, comments;
+
+    public Ticket(string id, string title, string description, string creationTime, string status,
+                  string mentorName, string creator, ArrayList<string> tags,
+                  ArrayList<string> comments) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.creationTime = creationTime;
+        this.status = status;
+        this.mentorName = mentorName;
+        this.creator = creator;
+        this.tags = tags;
+        this.comments = comments;
+    }
+
+    public ArrayList<Ticket> getTicketList() {
+        new ArrayList<Ticket> ticketes;
+
+        return tickets;
+    }
+
+
+
+    /**
+     * getter and setter hell below. Beware all who enter
+     */
+
+    public string getId() {
+        return id;
+    }
+
+    public void setId(string id) {
+        this.id = id;
+    }
+
+    public string getTitle() {
+        return title;
+    }
+
+    public void setTitle(string title) {
+        this.title = title;
+    }
+
+    public string getDescription() {
+        return description;
+    }
+
+    public void setDescription(string description) {
+        this.description = description;
+    }
+
+    public string getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(string creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public string getStatus() {
+        return status;
+    }
+
+    public void setStatus(string status) {
+        this.status = status;
+    }
+
+    public string getMentorName() {
+        return mentorName;
+    }
+
+    public void setMentorName(string mentorName) {
+        this.mentorName = mentorName;
+    }
+
+    public string getCreator() {
+        return creator;
+    }
+
+    public void setCreator(string creator) {
+        this.creator = creator;
+    }
+
+    public ArrayList<string> getTags() {
+        return tags;
+    }
+
+    public void setTags(ArrayList<string> tags) {
+        this.tags = tags;
+    }
+
+    public ArrayList<string> getComments() {
+        return comments;
+    }
+
+    public void setComments(ArrayList<string> comments) {
+        this.comments = comments;
+    }
+}
