bc82c9be5f5f58932c8d6bb6dd9857650ca75104
zachary <zacharysang@gmail.com>
2017-07-08 17:42:40 -0400

    Added edit endpoint

--- Web/api.js
+++ Web/api.js
@@ -79,6 +79,69 @@ router.use('/create',function(req,res){
 });
 
 /*
+Important, will need to fix flaw that allows user to overwrite any ticket if they have the id
+^Only protection is client-side validation (boo)
+
+Endpoint for editing a ticket. Takes a ticket object and overwrites the object with the given id
+
+URL: /api/edit
+
+    Method: POST
+
+    Body Parameters (~ --> optional):
+        * problemTitle
+        * ~problemDescription
+        * firstName
+        * ~lastName
+        * ~contactInfo
+        * location
+        * pin
+        * ~tags (Note: this is just a string separated by commas or spaces)
+        * ~mentorName
+
+    Success Response:
+        Code: 200
+        Content: success
+
+    Failure Response:
+        Code: 400
+        Content: <error message>
+
+ */
+router.use('/edit',function(req,res){
+    
+    if(!validateCreation(req.body)){
+        res.status(400).send('ticket invalid');
+    }
+
+    var body = req.body;
+
+    Ticket.update({ticketId: body.ticketId},
+    {
+        ticketId: body.ticketId,
+        problemTitle: body.problemTitle,
+        problemDescription: body.problemDescription,
+        creator: {firstName: body.firstName, 
+                lastName: body.lastName,
+                contactInfo: body.contactInfo,
+                location: body.location},
+        creationTime: body.creationTime,
+        pin: body.pin,
+        Tags: getTagsList(body.tags),
+        comments: body.comments,
+        status: body.status,
+        mentorName: body.mentorName
+    },function(err){
+        if(!err){
+            res.status(200).send('success');
+        } else {
+            res.status(400).send(err);
+        }
+    });
+
+});
+
+/*
     URL: /api/load
 
     Method: GET
@@ -89,7 +152,7 @@ router.use('/create',function(req,res){
 
     Success Response:
         Code: 200
-        Content: <requested tickets>
+        Content: <list of requested tickets>
 
     Failure Response:
         Code: 400
@@ -107,13 +170,9 @@ router.get('/load',function(req,res){
     });
 });
 
-
 // triggered for any get request to the api
 router.use('/*',function(req,res){
-    var responseObj = {
-        'foo': 'bar'
-    }
-    res.send(responseObj);
+    res.status(200).send('welcome to the api');
 });
 
 function getTagsList(str){
