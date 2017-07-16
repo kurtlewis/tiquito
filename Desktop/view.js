2085682432f3d7a8a7af467de964f8ef8f47dcea
Zach Collins <collinzy@mail.uc.edu>
2017-07-15 20:01:53 -0400

    implement single view

--- Desktop/view.js
+++ Desktop/view.js
@@ -1,71 +1,89 @@
-var req = new XMLHttpRequest();
 var tickets = [];
-req.open('GET', "http://test.tiquito.com/api/load", true);
-req.onreadystatechange = function(e) {
-    if (this.readyState == 4) {
-        var res = JSON.parse(req.responseText);
-        console.log(res);
-        tickets.concat(res);
-        var table = document.getElementById("res");
-        res.map(function(obj) {
-            var row = table.insertRow();
-            row.id = obj._id;
-            row.addEventListener("click", function(e) {onListClick(e, obj)});
-            var title = row.insertCell();
-            var tags = row.insertCell();
-            var status = row.insertCell();
-            console.log(obj);
-            title.innerHTML = obj.problemTitle;
-            tags.innerHTML = obj.Tags;
-            status.innerHTML = obj.status;
-        });
+var lastClicked = "";
+function load(offset) {
+    var req = new XMLHttpRequest();
+    req.open('GET', "http://test.tiquito.com/api/load", true);
+    req.onreadystatechange = function(e) {
+        if (this.readyState == 4) {
+            var res = JSON.parse(req.responseText);
+            console.log(res);
+            tickets.concat(res);
+            var listView = document.getElementById("listView");
+            if (offset == 0) {
+                listView.innerHTML = "";
+                var titlebar = `
+                <h2>All Tickets</h2>
+                <h3>Select one to view more details</h3>
+                <div id="titlebar">
+                    <span class="left">Title</span>
+                    <span class="right">Status</span>
+                    <span class="center">Tags</span>
+                </div>           
+                `
+                listView.innerHTML = titlebar;
+            }
+            res.map(function(obj) {
+                var row = document.createElement("div");
+                row.id = obj._id;
+                row.className = "ticket";
+                row.addEventListener("click", function(e) {onListClick(e, obj)});
+                var title = document.createElement("div");
+                var tags = document.createElement("div");
+                var status = document.createElement("div");
+                console.log(obj);
+                title.innerHTML = obj.problemTitle;
+                title.className = "ticketTitle";
+                tags.innerHTML = obj.Tags;
+                tags.className = "ticketTags";
+                status.innerHTML = obj.status;
+                status.className = "ticketStatus";
+                row.appendChild(title);
+                row.appendChild(tags);
+                row.appendChild(status);
+                listView.appendChild(row);
+            });
+        }
     }
+    req.send();
 }
-req.send();
+
+function makeEditable(item) {
+    item.contentEditable = "true";
+    console.log("here");
+}
 
 function onListClick(e, ticket) {
     console.log(ticket);
     ticketView = document.getElementById("ticketView");
     ticketView.innerHTML = "";
-    var para = document.createElement("p");
-    para.innerHTML = ticket.problemTitle;
-    ticketView.appendChild(para);
-    para = document.createElement("p");
-    para.innerHTML = "Name";
-    ticketView.appendChild(para);
-    para = document.createElement("p");
-    para.innerHTML = ticket.creator.firstName + " " + ticket.creator.lastName;
-    ticketView.appendChild(para);
-    para = document.createElement("p");
-    para.innerHTML = "Contact Info";
-    ticketView.appendChild(para);
-    para = document.createElement("p");
-    para.innerHTML = ticket.creator.contactInfo;
-    ticketView.appendChild(para);
-    para = document.createElement("p");
-    para.innerHTML = "Description";
-    ticketView.appendChild(para);
-    para = document.createElement("p");
-    para.innerHTML = ticket.problemDescription;
-    ticketView.appendChild(para);
-    para = document.createElement("p");
-    para.innerHTML = "Location";
-    ticketView.appendChild(para);
-    para = document.createElement("p");
-    para.innerHTML = ticket.creator.location;
-    ticketView.appendChild(para);
-    para = document.createElement("p");
-    para.innerHTML = "Status";
-    ticketView.appendChild(para);
-    para = document.createElement("p");
-    para.innerHTML = ticket.status;
-    ticketView.appendChild(para);
-    para = document.createElement("p");
-    para.innerHTML = "Mentor Name";
-    ticketView.appendChild(para);
-    para = document.createElement("p");
-    para.innerHTML = ticket.mentorName;
+    var html = `
+    <p>Title</p>
+    <p class="field" id="title" onmouseover="makeEditable(this)">${ticket.problemTitle}</p>
+    <p>Name</p>
+    <p class="field" id="name" onmouseover="makeEditable(this)">${ticket.creator.firstName + " " + ticket.creator.lastName}</p>
+    <p>Contact Info</p>
+    <p class="field" id="contactinfo" onmouseover="makeEditable(this)">${ticket.creator.contactInfo}</p>
+    <p>Description</p>
+    <p class="field" id="description" onmouseover="makeEditable(this)">${ticket.problemDescription}</p>
+    <p>Location</p>
+    <p class="field" id="location" onmouseover="makeEditable(this)">${ticket.creator.location}</p>
+    <p>Status</p>
+    <p class="field" id="status" onmouseover="makeEditable(this)">${ticket.status}</p>
+    <p>Mentor Name</p>
+    <p class="field" id="mentorname" onmouseover="makeEditable(this)">${ticket.mentorName}</p>
+    `
+    ticketView.innerHTML = html;
+    var prev = document.getElementById(lastClicked);
+    if (prev) {
+        prev.className = "ticket";
+    }
+    var current = document.getElementById(ticket._id);
+    current.className = "ticket highlight";
+    lastClicked = ticket._id;
 }
+
+// main
+load(0);
 
 /* Sample Ticket Here
 Tags:Array[2]
