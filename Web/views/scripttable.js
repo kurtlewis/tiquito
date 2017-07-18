e5cf9707e07ad6b64e5de86f1b8e5da33a9e2f03
James <zacherysparks53@gmail.com>
2017-07-18 08:38:52 -0400

    Up to detail view

--- Web/views/scripttable.js
+++ Web/views/scripttable.js
@@ -6,19 +6,15 @@ function append(parent, el){
     return parent.appendChild(el);
 }
 
-// var div = document.createElement("div");
-// div.style.width = "100px";
-// div.style.height = "100px";
-// div.style.background = "red";
-// div.style.color = "white";
-// div.innerHTML = "Hello";
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
 
-// document.body.appendChild(div);
-
-const table = document.getElementById('listItems');
-const url = '/api/load';
-
-fetch(url)
+function renderTable() {
+    fetch(url)
 .then((resp) => resp.json())
 .then(function(data) {
     let listItems = data;
@@ -31,6 +27,13 @@ fetch(url)
         // span.innerHTML = '${item.firstname}';
         // img.src = item.picture.medium;
         buttonlink.innerHTML = "View";
+        buttonlink.addEventListener('click', function(){
+            detailview(`${listItem._id}`);
+        })
+        // buttonlink.setAttribute('onclick','detailview();');
+        // buttonlink.setAttribute('onclick',"detailview(\''+`${listItem._id}`+'\');");
+        // buttonlink.onclick = "detailview(`${listItem._id}`);";
+        // buttonlink.onclick = 'detailview(' + `${listItem._id}` + ');';
         problemtitle.innerHTML = `${listItem.problemTitle}`;
         tags.innerHTML = `${listItem.Tags}`;
         ticketstatus.innerHTML = `${listItem.status}`;
@@ -45,4 +48,83 @@ fetch(url)
 .catch(function(error){
     console.log(error);
 })
+}
+
+function searchfunction(){
+    var searchquery = document.getElementById("searchbox").value;
+
+    window.location.href = '/list?search='+searchquery+'&';
+}
+
+var viewmore_offset = 0;
+
+function viewmore(){
+    viewmore_offset += 10;
+    url = '/api/load?';
+    url += 'offset=' + viewmore_offset + '&';
+    renderTable();
+    div.innerHTML = viewmore_offset;
+}
+
+function detailview(data){
+    div.innerHTML = data;
+    window.location.href = '/detail?ticketid='+data;
+}
+
+getUrlParameter('post'); // "1234"
+getUrlParameter('action'); // "edit"
+
+var div = document.createElement("div");
+div.style.width = "300px";
+div.style.height = "100px";
+div.style.background = "red";
+div.style.color = "white";
+div.innerHTML = url;
+document.body.appendChild(div);
+
+const table = document.getElementById('listItems');
+var url = '/api/load';
+
+var offset = getUrlParameter('offset');
+var limit = getUrlParameter('limit');
+var sort = getUrlParameter('sort');
+var direction = getUrlParameter('direction');
+var filter = getUrlParameter('filter');
+var search = getUrlParameter('search');
+
+if (url.includes('?'))
+{
+}else{
+    url +='?';
+}
+
+if (offset!=null && offset!='' && offset !=' ') {
+    // if (url.includes('offset')){
+    // }
+    ////Ok, so problem in the future is having more than one offset/whatever in url
+    ////not a problem now since the url does not persist
+    url +='offset='+offset+'&';
+}
+
+if (limit!=null && limit!='' && limit !=' ') {
+    url +='limit='+limit+'&';
+}
+
+if (sort!=null && sort!='' && sort !=' ') {
+    url +='sort='+sort+'&';
+}
+
+if (direction!=null && direction!='' && direction !=' ') {
+    url +='direction='+direction+'&';
+}
+
+if (filter!=null && filter!='' && filter !=' ') {
+    url +='filter='+filter+'&';
+}
+
+if (search!=null && search!='' && search !=' ') {
+    url +='search='+search+'&';
+}
+
+renderTable();
 
